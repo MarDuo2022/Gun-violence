19e7edf1e4f750d524a13d5a6bb02a079df763a0
MarDuo2022 <113748980+MarDuo2022@users.noreply.github.com>
2022-12-17 10:06:26 +0800

    table schema completed

---
+++ table_schema.sql
@@ -0,0 +1,40 @@
+CREATE TABLE mass_shooting_master (
+	Incident_ID int NOT NULL,
+	Incident_Date date NOT NULL,
+	State varchar NOT NULL,
+	City_county varchar NOT NULL,
+	Address varchar,
+	Killed int,
+	Injured int,
+	PRIMARY KEY (Incident_ID)
+);
+
+CREATE TABLE children_casualty (
+	Incident_ID int NOT NULL,
+	Incident_Date date NOT NULL,
+	State varchar NOT NULL,
+	City_county varchar NOT NULL,
+	Address varchar,
+	Killed int,
+	Injured int,
+	Record_type varchar NOT NULL,
+	PRIMARY KEY (Incident_ID),
+	FOREIGN KEY (Incident_ID) REFERENCES mass_shooting_master (Incident_ID)
+);
+
+CREATE TABLE teen_casualty (
+	Incident_ID int NOT NULL,
+	Incident_Date date NOT NULL,
+	State varchar NOT NULL,
+	City_county varchar NOT NULL,
+	Address varchar,
+	Killed int,
+	Injured int,
+	Record_type varchar NOT NULL,
+	PRIMARY KEY (Incident_ID),
+	FOREIGN KEY (Incident_ID) REFERENCES mass_shooting_master (Incident_ID)
+);
+
+
+
+    
